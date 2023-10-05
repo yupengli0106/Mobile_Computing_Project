558af9e0e4697a65f9c276f96093bd22b578e242
Yupeng Li <lypa520@gmail.com>
2023-10-05 15:55:54 +1100

    added basic login and register view

--- app/src/main/java/com/example/zenly/MainActivity.java
+++ app/src/main/java/com/example/zenly/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.zenly;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +13,15 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    /**
+     * Go to register activity when user clicks on register button
+     * @param view
+     */
+    public void goToRegister(View view) {
+        Intent intent = new Intent(this, RegisterActivity.class);
+        startActivity(intent);
+    }
+
+
 }
