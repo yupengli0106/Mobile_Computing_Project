77328a39d07aad495d250f4f5570bfb74196a1ed
Lipeng Zhang <723681494@qq.com>
2023-10-23 05:08:08 +1100

    feat(navigation): complete bottom navigation bar and fragment switching

--- app/src/main/java/com/example/activities/MainActivity.java
+++ app/src/main/java/com/example/activities/MainActivity.java
@@ -8,7 +8,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 
+import com.example.fragments.ChatFragment;
+import com.example.fragments.FriendsFragment;
 import com.example.fragments.MapFragment;
+import com.example.fragments.ProfileFragment;
 import com.example.zenly.R;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
@@ -17,6 +20,9 @@ public class MainActivity extends AppCompatActivity {
     private final FragmentManager fragmentManager = getSupportFragmentManager();
     // reuse mapFragment
     private final MapFragment mapFragment = new MapFragment();
+    private final ChatFragment chatFragment = new ChatFragment();
+    private final FriendsFragment friendsFragment = new FriendsFragment();
+    private final ProfileFragment profileFragment = new ProfileFragment();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,19 +35,21 @@ public class MainActivity extends AppCompatActivity {
         // set map fragment as default fragment
         fragmentManager.beginTransaction().replace(R.id.fragment_container, mapFragment).commit();
 
+        bottomNavigationView.setSelectedItemId(R.id.nav_map);
+
         //setOnNavigationItemSelectedListener
         bottomNavigationView.setOnItemSelectedListener(item -> {
             Fragment selectedFragment = null;
             int itemId = item.getItemId();
             if (itemId == R.id.nav_map) {
-//                selectedFragment = new MapFragment();
-                selectedFragment = mapFragment;  // Reuse mapFragment
+                selectedFragment = new MapFragment();
+            } else if (itemId == R.id.nav_chat) {
+                selectedFragment = chatFragment;
+            } else if (itemId == R.id.nav_profile) {
+                selectedFragment = profileFragment;
+            } else if (itemId == R.id.nav_friends) {
+                selectedFragment = friendsFragment;
             }
-//                else if (itemId == R.id.nav_chat) {
-//                    TODO: navigate to chat fragment
-//                } else if (itemId == R.id.nav_settings) {
-//                    TODO: navigate to settings fragment
-//                }
 
             if (selectedFragment != null) {
                 fragmentManager.beginTransaction().replace(R.id.fragment_container, selectedFragment).commit();
