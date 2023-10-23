3ef61630f7095e5242062939685f4bc48aa65021
Yupeng Li <lypa520@gmail.com>
2023-10-23 19:30:08 +1100

    adjust code structure

--- app/src/main/java/com/example/fragments/MapFragment.java
+++ app/src/main/java/com/example/fragments/MapFragment.java
@@ -14,12 +14,12 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.activities.LoginActivity;
-import com.example.model.LocationData;
 import com.example.zenly.R;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.firebase.auth.FirebaseUser;
@@ -33,8 +33,7 @@ import com.google.firebase.database.FirebaseDatabase;
 
 import com.google.android.gms.maps.model.Marker;
 
-
-import java.util.concurrent.ConcurrentHashMap;
+import java.util.HashMap;
 
 public class MapFragment extends Fragment {
     private final String TAG = "MapFragmentLog";
@@ -43,7 +42,9 @@ public class MapFragment extends Fragment {
     // ValueEventListener to listen for changes in the database
     private ValueEventListener myValueEventListener;
     // use a HashMap to store the markers of all users
-    private final ConcurrentHashMap<String, Marker> userMarkers = new ConcurrentHashMap<>();
+    private final HashMap<String, Marker> userMarkers = new HashMap<>();
+    // use a HashMap to store the usernames of all users to avoid querying the database multiple times
+    private final HashMap<String, String> usernameCache = new HashMap<>();
     // default zoom level of the map when the app is first loaded (street level)
     private final float DEFAULT_ZOOM_LEVEL = 15.0f;
     // Firebase Realtime Database reference to the locations node
@@ -118,7 +119,6 @@ public class MapFragment extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-
         DatabaseReference myDatabase = FirebaseDatabase.getInstance().getReference();
         FirebaseAuth myAuth = FirebaseAuth.getInstance();
         currentUser = myAuth.getCurrentUser();
@@ -143,13 +143,12 @@ public class MapFragment extends Fragment {
         }
     }
 
+    /**
+     * Start the repeating task to listen for changes in the database
+     */
     private void startRepeatingTask() {
         Log.d(TAG, "startRepeatingTask: start repeating task");
         myValueEventListener = new ValueEventListener() {
-            /**
-             * Called when data at a location changes.
-             * @param dataSnapshot The current data at the location
-             */
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
                 Log.d(TAG, "onDataChange: Total Users: " + dataSnapshot.getChildrenCount());
@@ -157,82 +156,10 @@ public class MapFragment extends Fragment {
                 for (DataSnapshot userSnapshot : dataSnapshot.getChildren()) { // traverse all users
                     Log.d(TAG, "onDataChange: User ID: " + userSnapshot.getKey());
                     Log.d(TAG, "onDataChange: User Data: " + userSnapshot.getValue());
-
-                    // get the location data of the user
-                    Double latitudeValue = userSnapshot.child("latitude").getValue(Double.class);
-                    double latitude = (latitudeValue != null) ? latitudeValue : 0.0;
-                    Double longitudeValue = userSnapshot.child("longitude").getValue(Double.class);
-                    double longitude = (longitudeValue != null) ? longitudeValue : 0.0;
-                    Float speedValue = userSnapshot.child("speed").getValue(Float.class);
-                    float speed = (speedValue != null) ? speedValue : 0.0f;
-                    Long timestampValue = userSnapshot.child("timestamp").getValue(Long.class);
-                    long timestamp = (timestampValue != null) ? timestampValue : 0L;
-                    Integer batteryLevelValue = userSnapshot.child("batteryLevel").getValue(Integer.class);
-                    int batteryLevel = (batteryLevelValue != null) ? batteryLevelValue : 0;
-
-                    // create a LocationData object
-                    LocationData locationData = new LocationData(latitude, longitude, speed, timestamp, batteryLevel);
-
-                    String userId = userSnapshot.getKey();
-                    if (userId != null) {
-                        // update the marker on the map if the user exists
-                        LatLng newLocation = new LatLng(locationData.getLatitude(), locationData.getLongitude());
-                        Marker existingMarker = userMarkers.get(userId); // find the existing marker
-
-                        // move the camera to the current user's location if when the user is logged in
-                        if(currentUser != null && userId.equals(currentUser.getUid())){
-                            if (isFirstLoad){
-                                myMap.moveCamera(CameraUpdateFactory.newLatLngZoom(newLocation, DEFAULT_ZOOM_LEVEL));
-                                isFirstLoad = false;
-                            }
-                        }
-
-                        // Get username from Users node
-                        DatabaseReference specificUserRef = userRef.child(userId);
-                        specificUserRef.addListenerForSingleValueEvent(new ValueEventListener() {
-                            @Override
-                            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                                // Get username
-                                String username = dataSnapshot.child("username").getValue(String.class);
-                                Log.d(TAG, "get username onDataChange: " + username);
-
-                                if (existingMarker != null) {// if the marker exists
-                                    // update the existing marker
-                                    existingMarker.setPosition(newLocation);
-                                    existingMarker.setTitle("User: " + username);
-                                    existingMarker.setSnippet("Speed: " + speed + "\n" + "Battery: " + batteryLevel+"%");
-                                    updateMapMarker(userId, newLocation, DEFAULT_ZOOM_LEVEL);
-                                } else {
-                                    // create a new marker
-                                    Marker newMarker = myMap.addMarker(new MarkerOptions()
-                                            .position(newLocation)
-                                            .title("User: " + username)
-                                            .snippet("Speed: " + speed + "\n" + "Battery: " + batteryLevel+"%"));
-
-                                    if (newMarker != null){
-                                        userMarkers.put(userId, newMarker);
-                                        updateMapMarker(userId, newLocation, DEFAULT_ZOOM_LEVEL);
-                                    }else {
-                                        Log.d(TAG, "onDataChange: newMarker is null");
-                                        Toast.makeText(getContext(), "Error fetching data", Toast.LENGTH_SHORT).show();
-                                    }
-
-                                }
-                            }
-
-                            @Override
-                            public void onCancelled(@NonNull DatabaseError databaseError) {
-                                Log.d(TAG, "get username onCancelled: " + databaseError.getMessage());
-                            }
-                        });
-                    }
+                    handleNewLocation(userSnapshot);
                 }
             }
 
-            /**
-             * Called when the read is cancelled.
-             * @param databaseError A description of the error that occurred
-             */
             @Override
             public void onCancelled(@NonNull DatabaseError databaseError) {
                 // Log error message
@@ -246,47 +173,101 @@ public class MapFragment extends Fragment {
         }
     }
 
+
+    /**
+     * Handle new location of a user
+     * @param userSnapshot snapshot of the user
+     */
+    private void handleNewLocation(@NonNull DataSnapshot userSnapshot) {
+        // get the user ID
+        String userId = userSnapshot.getKey();
+
+        // get the user's location
+        Double latitudeValue = userSnapshot.child("latitude").getValue(Double.class);
+        double latitude = (latitudeValue != null) ? latitudeValue : 0.0;
+        Double longitudeValue = userSnapshot.child("longitude").getValue(Double.class);
+        double longitude = (longitudeValue != null) ? longitudeValue : 0.0;
+        Float speedValue = userSnapshot.child("speed").getValue(Float.class);
+        float speed = (speedValue != null) ? speedValue : 0.0f;
+        Long timestampValue = userSnapshot.child("timestamp").getValue(Long.class);
+        long timestamp = (timestampValue != null) ? timestampValue : 0L;// TODO: last update time
+        Integer batteryLevelValue = userSnapshot.child("batteryLevel").getValue(Integer.class);
+        int batteryLevel = (batteryLevelValue != null) ? batteryLevelValue : 0;
+
+        // create a LatLng object from the latitude and longitude values
+        LatLng newLocation = new LatLng(latitude, longitude);
+
+        // get the username of the user
+        DatabaseReference specificUserRef = userRef.child(userId);
+        specificUserRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                //TODO: TBD if the username can be changed and need to update the usernameCache
+                String username = usernameCache.get(userId);
+                if (username == null) {
+                    username = dataSnapshot.child("username").getValue(String.class);
+                    usernameCache.put(userId, username);
+                }
+                // update the marker on the map after getting the username
+                updateMapMarker(userId, newLocation, DEFAULT_ZOOM_LEVEL, username, speed, batteryLevel);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                Log.d(TAG, "get username onCancelled: " + databaseError.getMessage());
+            }
+        });
+    }
+
+
     /**
      * Update the marker on the map
-     * @param userId the user id
-     * @param newLocation the new location of the user
-     * @param zoomLevel the zoom level of the map
+     * @param userId the user ID
+     * @param newLocation new location of the user
+     * @param zoomLevel zoom level of the map
+     * @param username username of the user
+     * @param speed speed of the user
+     * @param batteryLevel battery level of the user's device
      */
-    private void updateMapMarker(String userId, LatLng newLocation, float zoomLevel) {
+    private void updateMapMarker(String userId, LatLng newLocation, float zoomLevel, String username, float speed, int batteryLevel) {
         if (myMap != null) {
             Marker existingMarker = userMarkers.get(userId); // get the existing marker
             if (existingMarker != null) {
-                Log.d(TAG, "Updating marker for user: " + userId);
                 existingMarker.setPosition(newLocation); // update the existing marker's position
+                existingMarker.setTitle("User: " + username);
+                existingMarker.setSnippet("Speed: " + speed + "\n" + "Battery: " + batteryLevel + "%");
+                existingMarker.setIcon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_GREEN));
             } else {
-                Log.d(TAG, "Creating new marker for user: " + userId);
-                Marker newMarker = myMap.addMarker(new MarkerOptions().position(newLocation).title("User " + userId));
-                if (newMarker != null){
-                    userMarkers.put(userId, newMarker); // add the new marker to the HashMap
-                }else {
-                    Log.d(TAG, "updateMapMarker: newMarker is null");
-                    Toast.makeText(getContext(), "Error fetching data", Toast.LENGTH_SHORT).show();
-                }
+                Marker newMarker = myMap.addMarker(new MarkerOptions()
+                        .position(newLocation)
+                        .title("User: " + username)
+                        .snippet("Speed: " + speed + "\n" + "Battery: " + batteryLevel + "%"));
+                userMarkers.put(userId, newMarker);
             }
-
-            // Only move and zoom the camera during the first load
-            if (isFirstLoad) {
+            if (currentUser != null && userId.equals(currentUser.getUid()) && isFirstLoad) {
                 myMap.moveCamera(CameraUpdateFactory.newLatLngZoom(newLocation, zoomLevel));
-                isFirstLoad = false; // set it to false so that it won't zoom in again
+                isFirstLoad = false;
             }
         }
     }
 
-    /**
-     * Stop the repeating task when the activity is destroyed
-     */
+    @Override
+    public void onResume() {
+        super.onResume();
+        // start the repeating task again when the app is resumed
+        // user switches back to the app
+        isFirstLoad = true;
+    }
+
     @Override
     public void onDestroy() {
         super.onDestroy();
-        // Remove the listener using the member variable
-        if (myValueEventListener != null && locationsRef != null) {
-            locationsRef.removeEventListener(myValueEventListener);
-        }
+        // Remove the listener using the member variable to avoid memory leaks
+        // foreground service will keep running even if the app is closed
+        //TODO: foreground service will be stopped when the user logs out
+//        if (myValueEventListener != null && locationsRef != null) {
+//            locationsRef.removeEventListener(myValueEventListener);
+//        }
     }
 
 }
