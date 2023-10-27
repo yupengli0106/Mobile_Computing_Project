19fac7ca53ba5a7958375f7bc61e0096379d35dd
Yupeng Li <lypa520@gmail.com>
2023-10-27 23:41:51 +0800

    remove user password from database for safety reason (Firebase Authentication already handled user's password)

--- app/src/main/java/com/example/model/User.java
+++ app/src/main/java/com/example/model/User.java
@@ -3,16 +3,14 @@ package com.example.model;
 public class User {
     public String username;
     public String email;
-    public String password;
 
     public User() {
         // Default constructor is required for Firebase
     }
 
-    public User(String username, String email, String password) {
+    public User(String username, String email) {
         this.username = username;
         this.email = email;
-        this.password = password;
     }
     public String getUsername() {
         return username;
@@ -28,14 +26,6 @@ public class User {
 
     public void setEmail(String email) {
         this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
     }
 
 
